7f3dc41629ae435d6e60413a14ac08f93560e4af
RIngerslev <101323949+RIngerslev@users.noreply.github.com>
2022-09-15 11:31:40 +0200

    Initial Commit 3

--- src/app/HelloWorld.java
+++ src/app/HelloWorld.java
@@ -5,5 +5,6 @@ public class HelloWorld {
         System.out.println("Hello Rasmus!! ");
         System.out.println("Hello again");
         System.out.println("Hola");
+        System.out.println("Hola Rikke :D");
     }
 }
