1e513a22b32af92c897595f67d6e7b948dc04040
RIngerslev <101323949+RIngerslev@users.noreply.github.com>
2022-09-15 12:05:04 +0200

    Initial Commit 5

--- src/app/HelloWorld.java
+++ src/app/HelloWorld.java
@@ -1,5 +1,10 @@
 package app;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello Rasmus!! ");
@@ -8,5 +13,13 @@ public class HelloWorld {
         System.out.println("Hvad sker der??");
         System.out.println("Peter er den bedste");
         System.out.println("nyt");
+        List<String> navne = new ArrayList<>();
+        Collections.addAll(navne,"Jan Jensen", "Jens Hansen", "Leo Hansen", "Jens Jensen", "Jensine Larsen");
+        System.out.println(navne);
+    }
+
+    public static int countDelStreng(List<String> list, String sub){
+
+        return 0;
     }
 }
